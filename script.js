548e73cb51eae1e849ad421e8ec8d5d3d5c35853
Semyon <sema.chel12@gmail.com>
2024-08-20 14:34:05 +0300

    animation lvl pro

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-//взять холст
 const canvas = document.getElementById('canvas1');
-
-//метод рисования
 const ctx = canvas.getContext('2d');
-
-//размеры холста
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
-
-//вставка изображения
 const playerImage = new Image();
 playerImage.src = 'src/img/shadow_dog.png';
-//ширина 1 спрайта
 const spriteWidth = 575;
-// высоат 1 спрайта
 const spriteHeight = 523;
-//смена спрайтов
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 4;
 
+
+
 function animate() {
-	//очистить холст
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-	//изображение которое хочу нарисовать и их координаты x y
+	let position = Math.floor(gameFrame / staggerFrames) % 6;
+	frameX = spriteWidth * position;
 	ctx.drawImage(
 		playerImage,
-		frameX * spriteWidth,
+		frameX,
 		frameY * spriteHeight,
 		spriteWidth,
 		spriteHeight,
@@ -36,15 +28,9 @@ function animate() {
 		spriteWidth,
 		spriteHeight
 	);
-	if (gameFrame % staggerFrames == 0) {
-		if (frameX < 6) frameX++;
-		else frameX = 0;
-	}
-	//меняем frame и тем самым создаём анимацию
 
 	gameFrame++;
 
-	//запуск рекурсии с отрисовкой фигуры
 	requestAnimationFrame(animate);
 }
 animate();
